2f6a77f4a95065e2e900e52fed759bf521b9814f
Tom Perez <tom.perez2@yahoo.com>
2011-06-08 23:21:18 -0400

    added links to two separate pages and configured root to go to player listings

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,8 @@ Rankme::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
+  root :to => "players#index"
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
