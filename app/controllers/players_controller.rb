0a15d63370c215a94503c9e2eace33fed7b2709d
Tom Perez <tom.perez2@yahoo.com>
2011-06-17 01:01:06 -0400

    sort players by rating descending

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,7 +2,7 @@ class PlayersController < ApplicationController
   # GET /players
   # GET /players.xml
   def index
-    @players = Player.all
+    @players = Player.all.sort_by {|player| player.rating}.reverse
 
     respond_to do |format|
       format.html # index.html.erb
